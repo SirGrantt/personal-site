90539064bf0e3c7a618d2b5730665fb28bccc706
SirGrantt <sirgrantt@gmail.com>
2019-01-08 22:55:47 -0600

    adding in template and inital changes for personal site implementation

---
+++ src/components/Header.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import styled from 'react-emotion';
+import Img from 'gatsby-image';
+import PropTypes from 'prop-types';
+import profilePic from '../../static/logo/profile-pic.jpg';
+
+const Wrapper = styled.header`
+  -webkit-clip-path: polygon(100% 0, 0 0, 0 70%, 50% 100%, 100% 70%);
+  clip-path: polygon(100% 0, 0 0, 0 70%, 50% 75%, 100% 70%);
+  @media (max-width: ${props => props.theme.breakpoints.s}) {
+    -webkit-clip-path: polygon(100% 0, 0 0, 0 90%, 50% 100%, 100% 90%);
+    clip-path: polygon(100% 0, 0 0, 0 90%, 50% 100%, 100% 90%);
+  }
+  background: ${props => props.theme.gradient.rightToLeft};
+  height: 300px;
+  @media (max-width: ${props => props.theme.breakpoints.m}) {
+    height: 300px;
+  }
+  @media (max-width: ${props => props.theme.breakpoints.s}) {
+    height: 275px;
+  }
+  position: relative;
+  overflow: hidden;
+`;
+
+const Pic = styled.img`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center center;
+  opacity: 1;
+  transition: opacity 0.5s ease 0s;
+  border-radius: 50%;
+`;
+
+const PicWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+`;
+
+const StyDiv = styled.div`
+  width: 100%;
+  padding-bottom: 100%;
+`;
+
+const Text = styled.div`
+  color: ${props => props.theme.colors.white.base};
+  z-index: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 100%;
+  max-width: ${props => props.theme.layout.base};
+  padding: 0 2rem;
+  margin-bottom: 3rem;
+  align-items: center;
+`;
+
+const Subtitle = styled.p`
+  max-width: 650px;
+  color: ${props => props.theme.colors.white.light};
+`;
+
+const Header = ({ children, title, job, cover }) => (
+  <Wrapper>
+    {/* <Img fluid={cover || {} || [] || ''} /> */}
+    <PicWrapper>
+      <StyDiv>
+      <Pic src={profilePic} alt="Grant Elmer" />
+      </StyDiv>
+    </PicWrapper>
+    <Text>
+      <h1>{title}</h1>
+      <h5>{job}</h5>
+      {children && <Subtitle>{children}</Subtitle>}
+    </Text>
+  </Wrapper>
+);
+
+export default Header;
+
+Header.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.bool]),
+  cover: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  job: PropTypes.string,
+  title: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
+};
+
+Header.defaultProps = {
+  children: false,
+  cover: false,
+  date: false,
+  title: false,
+};
